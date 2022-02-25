79f62c4f5e0dbecd9cb045d346b3b62c84793ee1
apimento <98665437+apimento@users.noreply.github.com>
2022-02-25 18:57:15 -0500

    p1 assessment done

---
+++ script.js
@@ -0,0 +1,39 @@
+//* VARIABLES *// 
+
+
+//* CACHED ELEMENTS *// 
+let runningTotal = document.querySelector("#runningTotal");
+const addButton = document.querySelector("#addButton"); 
+let inputBox = document.querySelector("#inputBox"); 
+const subButton = document.querySelector("#subtractButton"); 
+
+
+
+//* EVENT LISTENERS *// 
+addButton.addEventListener( "click" , function(e){ 
+    console.log(inputBox.value , runningTotal.innerText);
+    const num1 = parseInt(inputBox.value); 
+    const num2 = parseInt(runningTotal.innerText);
+    let sum = num1 + num2;
+    runningTotal.innerText = sum;
+})
+
+
+
+subButton.addEventListener( "click" , function(e){ 
+    console.log(inputBox.value , runningTotal.innerText);
+    const num1 = parseInt(inputBox.value); 
+    const num2 = parseInt(runningTotal.innerText);
+    let difference = num2 - num1;
+    runningTotal.innerText = difference; 
+    if (difference < 0 ){ 
+        runningTotal.style.color = "red";
+    } else { 
+        runningTotal.style.color = "black";
+    }
+}) 
+
+
+
+
+
