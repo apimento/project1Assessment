f6bf0e72a47788f33effd09b2f468f9797798b41
apimento <98665437+apimento@users.noreply.github.com>
2022-02-25 19:21:27 -0500

    housekeeping

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-//* VARIABLES *// 
-
-
 //* CACHED ELEMENTS *// 
 let runningTotal = document.querySelector("#runningTotal");
 const addButton = document.querySelector("#addButton"); 
 let inputBox = document.querySelector("#inputBox"); 
 const subButton = document.querySelector("#subtractButton"); 
-
 
 
 //* EVENT LISTENERS *// 
@@ -16,8 +12,12 @@ addButton.addEventListener( "click" , function(e){
     const num2 = parseInt(runningTotal.innerText);
     let sum = num1 + num2;
     runningTotal.innerText = sum;
+    if (sum < 0 ){ 
+        runningTotal.style.color = "red";
+    } else { 
+        runningTotal.style.color = "black";
+    }
 })
-
 
 
 subButton.addEventListener( "click" , function(e){ 
